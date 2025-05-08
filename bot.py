21d4d5241fd8af239109cf3581bef7166673ae6a
SergiOningeR <gsa@certus-telecom.ru>
2025-05-08 14:04:59 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
-# Основной запуск бота
-from aiogram import executor
-from handlers import user, admin, group
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+from loader import dp
+import handlers.user
+import handlers.admin
+import handlers.group
+from app.database import init_db
+
+init_db()
 
 if __name__ == '__main__':
-    executor.start_polling(...)  # Упростим для примера
+    executor.start_polling(dp, skip_updates=True)
---
+++ loader.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
